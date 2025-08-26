35677c81859807bf0b4165630413bebcc861effe
nerdyamin <aminualee02@hotmail.com>
2025-08-26 23:17:13 +0100

    testing live

--- Shared/Helpers/Constants.cs
+++ Shared/Helpers/Constants.cs
@@ -4,6 +4,6 @@ namespace Shared.Helpers;
 public class Constants
 {
     public static string Url = "AppUrl";
-    public static string? ProdBaseAddress = "https://demo-mcc.onrender.com/api/";
+    public static string? ProdBaseAddress = "https://atlanticlogistics-atv.com.ng/api/";
     public static string? DevBaseAddress =  "https://localhost:7229/api/";    
 }
