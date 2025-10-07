d0e65d836e37975d47f201e8802694ee18d95102
nerdyamin <aminualee02@hotmail.com>
2025-10-07 21:31:20 +0100

    updated

--- Api/Controllers/AppVersionController.cs
+++ Api/Controllers/AppVersionController.cs
@@ -14,14 +14,14 @@ public class AppVersionController : ControllerBase
         #if DEBUG
             AppVersion = 1.1;
         #else
-            AppVersion = 2.2;
+            AppVersion = 2.3;
         #endif
         return Ok(new VersionManifest
         {
             AppVersion = AppVersion, // Update this with each release
             ForceUpdate = true,    // Set to true when critical updates are available
             ReleaseDate = DateTime.Now,
-            ReleaseNotes = "bug fixes"
+            ReleaseNotes = @"bug fixes"
         });
     }
 }
