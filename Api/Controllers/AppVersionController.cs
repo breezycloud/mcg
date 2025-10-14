4fac17f09e5b45eb074bb6c14313a45c90abd6f1
nerdyamin <aminualee02@hotmail.com>
2025-10-14 17:40:54 +0100

    new user permission

--- Api/Controllers/AppVersionController.cs
+++ Api/Controllers/AppVersionController.cs
@@ -14,14 +14,14 @@ public class AppVersionController : ControllerBase
         #if DEBUG
             AppVersion = 1.1;
         #else
-            AppVersion = 2.4;
+            AppVersion = 2.5;
         #endif
         return Ok(new VersionManifest
         {
             AppVersion = AppVersion, // Update this with each release
             ForceUpdate = true,    // Set to true when critical updates are available
             ReleaseDate = DateTime.Now,
-            ReleaseNotes = @"updated dashboard shortage analysis"
+            ReleaseNotes = "added new user permission on edit trip"
         });
     }
 }
