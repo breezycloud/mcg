0b93f45c9b17e05f12b2b0b770ac4d8892cebdaa
nerdyamin <aminualee02@hotmail.com>
2025-11-11 10:43:27 +0100

    update

--- Api/Controllers/AppVersionController.cs
+++ Api/Controllers/AppVersionController.cs
@@ -13,7 +13,7 @@ public class AppVersionController : ControllerBase
         #if DEBUG
             AppVersion = 1.1;
         #else
-            AppVersion = 3.3;
+            AppVersion = 3.4;
         #endif
         
         return Ok(new VersionManifest
